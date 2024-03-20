b0b6d8cb8ac001bd96c52251585b2e205e959c4e
boon <bkiat1123@gmail.com>
2024-03-20 15:10:17 +0800

    Add claude 3 haiku

--- llms/providers/anthropic.py
+++ llms/providers/anthropic.py
@@ -25,6 +25,7 @@ class AnthropicProvider(BaseProvider):
         },
         "claude-instant-1.2": {"prompt": 1.63, "completion": 5.51, "token_limit": 100_000, "output_limit": 4_096},
         "claude-2.1": {"prompt": 8.00, "completion": 24.00, "token_limit": 200_000, "output_limit": 4_096},
+        "claude-3-haiku-20240307": {"prompt": 0.25, "completion": 1.25, "token_limit": 200_000, "output_limit": 4_096},
         "claude-3-sonnet-20240229": {"prompt": 3.00, "completion": 15, "token_limit": 200_000, "output_limit": 4_096},
         "claude-3-opus-20240229": {"prompt": 15.00, "completion": 75, "token_limit": 200_000, "output_limit": 4_096},
     }
