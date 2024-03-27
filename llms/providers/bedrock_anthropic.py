22851f3f2af7409004ae1b1e6e084e9a454d2392
boon <bkiat1123@gmail.com>
2024-03-27 12:05:01 +0800

    Add claude 3 to bedrock provider

--- llms/providers/bedrock_anthropic.py
+++ llms/providers/bedrock_anthropic.py
@@ -17,6 +17,8 @@ class BedrockAnthropicProvider(AnthropicProvider):
             "token_limit": 100_000,
         },
         "anthropic.claude-v2": {"prompt": 11.02, "completion": 32.68, "token_limit": 100_000},
+        "anthropic.claude-3-haiku-20240307-v1:0": {"prompt": 0.25, "completion": 1.25, "token_limit": 200_000, "output_limit": 4_096},
+        "anthropic.claude-3-sonnet-20240229-v1:0": {"prompt": 3.00, "completion": 15, "token_limit": 200_000, "output_limit": 4_096},
     }
 
     def __init__(
@@ -54,3 +56,7 @@ class BedrockAnthropicProvider(AnthropicProvider):
             aws_region=aws_region,
             **async_client_kwargs,
         )
+
+    @property
+    def support_message_api(self):
+        return "claude-3" in self.model
