67f3957ef0237d1593416747caee983cf42c7287
VP <vlad@kagi.com>
2024-07-26 09:21:49 -0700

    Implemented Easter date calculation within the Gregorian Calendar.

--- llms/llms.py
+++ llms/llms.py
@@ -180,7 +180,6 @@ class LLMS:
             "Oslo"),
             ("write a sentence about trees with no words beginning with the letter t",
             "Correct answer will have no words begin with the letter t"),
-            (
             ("write 7 numbers between 10 and 110. none of them should begin with 1,5,3,4,2,6,8 or 7",
             "Correct answer will have 7 numbers and none of them will begin with 1, 2, 3, 4, 5, 6, 7, 8"),
             ('If a + b + c = 30 and b = 10 and c = 5. Is a = 20? Answer only ”My answer is yes.” or ”My answer is no.” or ”My answer is maybe.”',
@@ -216,6 +215,104 @@ print the output""","...... ,, , ...... ..."),
                     What does this code do in one sentence?""",
                     'prints "Hello World" to the console',
                 ),
+                ("""#define _POSIX_SOURCE
+#include                                <time.h>
+#include                               <stdio.h>
+#define                             extern/* ioccc*/
+#define                           condition 22+d+e> 31
+#define                         declarator main ( void )
+#define                       keyword ae(t,d+e-9); ae(m,4)
+#define                     false ae (t, d+ e+ 22); ae (m,3)
+#define                   syntax_error(n); } else if (J < n) {
+#define                 relational_expression(i, o, ccc) i o ccc
+#define               errno translation_unit/*Apollo 13*/int errno
+#define             iterative_statement  for  (  expressions ) block
+#define             translation_unit  declarator  compound_statement
+#define             ae(unary_expression,ae)     unary_expression= ae
+#define             declaration char T[16];     struct tm ae(s,{ 0})
+#define             if_part {macrolist list     ae(b,J%4); type_name
+#define             tokens if (t==25&&d==28     &&a > 10) ae (t, 18)
+#define             selection_statement(a,b,c) if(a){b; } else{ c; }
+#define             storage_class ae(e,(2*b+4*c+6*d+N)%7); statement
+#define             statement_list iterative_statement return'^'^'^'
+#define            macro(x,y,cast) ae(M,x); ae(N,y)syntax_error(cast)
+#define           block { if(relational_expression(J,<,1700))if_part;}
+#define          declaration_list int J,a,b,c,d, e,t,m,M,N; declaration
+#define         true keyword; selection_statement(t==26,ae(t,19),tokens)
+#define        compound_statement { declaration_list ; statement_list ; }
+#define       expressions ae(J,1582); relational_expression(J,<,2200); ++J
+#define      list macro (24, 5, 2200) ae(M, 24); ae(N, 6); } ae(a, J % 19);
+#define     type_name ae (c, J % 7);ae (d, (19 * a + M) % 30); storage_class
+#define    statement  selection_statement ( condition,true,false)pptoken cast
+#define   macrolist macro(22,2,1800)macro     (23, 3,1900) macro (23, 4, 2100)
+#define  pptoken ae(s.tm_year,J-1900);ae       (s.tm_mon,m-1); ae(s.tm_mday,t);
+#define cast (void)strftime(T,sizeof T,         "%a %b %d %Y",&s),(void)puts(T);
+#include                               <errno.h>
+
+what does this do, in one sentence?""",
+"Calculate Easter dates within the Gregorian Calendar."),
+("""%:define _POSIX_SOURCE
+#include<fcntl.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#define D(N,t)Z t*N V<%t*z U M0*z); H z)u z; X}
+#define k(x,y)x<0||fstat(x,&y)||
+#define h(x)=open(x,O_RDONLY)
+#define b(x),(int)x.st_nlink
+#define B ;typedef g
+#define X exit (1);
+#define O .st_size
+#define U =malloc(
+#define Y S.st_ino
+#define v ;%>else
+#define W .st_dev
+#define o ||read(
+#define Z static
+#define g struct
+#define u return
+#define I char*
+#define V (M2)
+#define H if(
+#define _ ->
+
+/* HE WHO SAYS */
+
+Z I A<:32767/      M0(I )]; Z g     stat S,T; Z        size_t    y B f{
+I n ; g f *  x     ; dev_t d  ;    ino_t i; } f B      t{ M1     s,c; f
+*l; g t*L,*R; }    t; D(a,t)D(E    ,f)Z t*J(t*p,I      n){ H!   p){ p=
+a(); p    _ s =S      O; p _       c=1; p              _ L=p    _ R=0;
+p _ l=    E(); p      _ l  _       n=n; p              _ l _   x=0; p
+_ l  _    d=S W;      p _  l       _ i= Y              v H S   O==p _
+s){ f*    e; for      (e=p _       l; e; e=e _ x)      { H S W==e _ 
+d&&Y==    e _ i)      { u p;        } } e=E(); e _     x=p _ l; e _ 
+n=n; e    _ d =S      W; e _                i=Y; p     _ l=e;  ++p _ 
+c v  H    S O< p      _ s) {                p _ L=     J( p _  L,n)v{
+p _ R=    J (p _      R,n );                } u  p     ; }  Z   int Q(
+I G,I F){ int d    h(G),D h(F);    I m,*M; H k(d,S     )k(D,T   )(y =S 
+O)-T O){ y= 0;     goto d; } H!    (m U y))||!(M U     y))o d    ,m,y)-
+y o D,M,y)-y)      X y=!memcmp(     m,M,y); M5(m)      ; M5(M    ); d:V
+
+ close (d );V      close(D); u        y; } Z M2 C(M1       z,M1 N){ M1     i=N*(N-1)/2,
+j=1,s; I q,*e,*    p,*w,*l; e=q=     M4((size_t)i,1);     H!e) X p=q+i;    for(i=0; e-p
+; ++e){ H!*e&&Q    (A[i:>,A[j])){   V printf("%""l""d"   "\t""%" "s""\t"   "%""s"
+"??/t"             "%""c"   "\11"   "%""d"      "??/t"   "%""d"    "\n",   z,A[i]
+,A[j],             S W -T   W?'X'   :'='b(      S)b(T)   ); H j    -i-1)   { s=N-
+i-3; w             =e+s+1; l=q+N*   (j-1)-      j*(j-1   )/ 2 ;            do{ *w
+=1; H w==l)        break; w+= s;    } while( s-->0); }   } H++j            ==N){ j=i+++
+ 2; } } M5(q);     } Z M2 P(t*p     ){ H p){ P(p _ R);   H  p _            c>1){ M1 i=0
+         ; f*l=    p _ l;           for (;      i< p _   c; ++i            ){ A[i
+         ]= l _    n; l=l           _ x; }      C (p _   s, p _    c); }   P (p _ 
+         L) ; }    }  int           main V      { t*r=   0; I F    ; for   (; ; )
+{ H!(F U 1024))    )X H !           fgets(      F,1024   ,stdin) )break;   *(F+(y
+=strlen(F))-1)=    0; H!(          F=M3(F,      y)))X H   stat(F,&S)==0    &&S_ISREG(S.
+ st_mode)&&S O     )r=J(r          ,F ); }      H r)P(r    ); u 0; }/*     Obfuscated C
+
+IS FREE THINKS MONEY GROWS ON DIRECTORY TREE */""",
+"program that finds and reports duplicate files"),
                 (
                     """#include <stdio.h> 
 
