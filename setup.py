0ee119d0f7d66566b1f29122ef102dcd0346831a
Vladimir Prelovac <vprelovac@gmail.com>
2025-02-27 13:07:56 -0800

    new

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ _project_homepage = "https://github.com/kagisearch/pyllms"
 
 setup(
     name="pyllms",
-    version="0.7.1",
+    version="0.7.2",
     description="Minimal Python library to connect to LLMs (OpenAI, Anthropic, Google, Mistral, OpenRouter, Reka, Groq, Together, Ollama, AI21, Cohere, Aleph-Alpha, HuggingfaceHub), with a built-in model performance benchmark.",
     long_description=long_description,
     long_description_content_type="text/markdown",
